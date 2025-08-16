b20a6233f9b73c596c93b49da5753322d39b83ca
David Magerl <dtm5011@gmail.com>
2025-08-15 21:50:52 -0700

    update astro config for pages env

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-username.github.io',
+  site: 'https://dtm5011.github.io',
+  base: import.meta.env.PROD ? '/find-local-chess' : '',
 });
